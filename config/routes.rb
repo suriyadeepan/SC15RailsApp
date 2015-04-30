3411be617965b9882dcacccc26b4fa6cf8cd4e61
Mohamed Saleem <mohsal.saleem@gmail.com>
2015-04-30 12:10:38 +0530

    Added contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :contacts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
