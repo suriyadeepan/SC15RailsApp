610bf2bd5e7145c8a04194d80219308337d3bdac
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2015-05-10 20:42:30 +0530

    added changes to make rack cors work for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -106,11 +106,4 @@ ActiveRecord::Schema.define(version: 20150509155351) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "send_mails", force: :cascade do |t|
-    t.string   "subject"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
